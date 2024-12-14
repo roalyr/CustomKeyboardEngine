c7f4bc658aba54cd8bf178db5b47cdfc1f01e81a
Roman Yeremenko <roal.yr@gmail.com>
2024-12-14 12:03:03 +0200

    Implemented basic main activity

--- app/src/main/kotlin/com/roalyr/new7rowkb/MainActivity.kt
+++ app/src/main/kotlin/com/roalyr/new7rowkb/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.roalyr.new7rowkb
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.roalyr.new7rowkb.ui.theme.New7rowKBTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +35,55 @@ class MainActivity : ComponentActivity() {
         setContent {
             New7rowKBTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Button(onClick = { openInputMethodSettings() }) {
+                            Text("Change Input Method")
+                        }
+                        Spacer(modifier = Modifier.height(32.dp))
+
+                        val annotatedString = buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    textDecoration = TextDecoration.Underline,
+                                    color = Color.Blue,
+                                    fontSize = 18.sp
+                                )
+                            ) {
+                                pushStringAnnotation(tag = "URL", annotation = "https://github.com/roalyr/New7rowKB")
+                                append("https://github.com/roalyr/New7rowKB")
+                                pop() // Don't forget to pop the annotation
+                            }
+                        }
+
+                        ClickableText(
+                            text = annotatedString,
+                            onClick = { offset ->
+                                annotatedString.getStringAnnotations(tag = "URL", start = offset, end = offset)
+                                    .firstOrNull()?.let { annotation ->
+                                        openUrl(annotation.item) 
+                                    }
+                            }
+                        )
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun openUrl(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    New7rowKBTheme {
-        Greeting("Android")
+    private fun openInputMethodSettings() {
+        val intent = Intent(Settings.ACTION_INPUT_METHOD_SETTINGS)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
     }
 }
